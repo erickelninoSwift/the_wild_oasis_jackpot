c52a55870237cadb114521c5a521591e4c09d0b6
Erick El nino <erick.tshimbombo@yahoo.com>
2024-01-04 03:55:50 +0200

    managed to add style components

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,25 @@
+import styled from "styled-components";
+import GlobalStyles from "./styles/GlobalStyles";
+
+const H1 = styled.h1`
+  font-size: 45px;
+  font-weight: 600;
+  color: red;
+  background-color: yellow;
+  padding: 10px;
+`;
+
+const StyledApp = styled.div`
+  background-color: red;
+  padding: 20px;
+`;
 function App() {
   return (
     <>
-      <div>Hello world</div>
+      <GlobalStyles />
+      <StyledApp>
+        <H1>Hello Guys Jackpot here</H1>
+      </StyledApp>
     </>
   );
 }
