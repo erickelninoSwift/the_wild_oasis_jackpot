0792838fdad9b9eda401273f4a106543798ccd47
Erick El nino <erick.tshimbombo@yahoo.com>
2024-01-05 06:18:01 +0200

    working on the applyout page

--- src/App.jsx
+++ src/App.jsx
@@ -13,22 +13,25 @@ import Account from "./pages/Account";
 import Settings from "./pages/Settings";
 import NewUsers from "./pages/Users";
 import GlobalStyles from "./styles/GlobalStyles";
-
+import AppLayout from "./AppLayout";
 function App() {
   return (
     <>
       <GlobalStyles />
       <Router>
         <Routes>
-          <Route index element={<Navigate replace to="dashboard" />} />
-          <Route path="dashboard" element={<Dashboard />} />
-          <Route path="login" element={<Login />} />
-          <Route path="booking" element={<Bookings />} />
-          <Route path="setting" element={<Settings />} />
-          <Route path="cabins" element={<Cabins />} />
+          <Route element={<AppLayout />}>
+            <Route index element={<Navigate replace to="dashboard" />} />
+            <Route path="dashboard" element={<Dashboard />} />
+
+            <Route path="booking" element={<Bookings />} />
+            <Route path="setting" element={<Settings />} />
+            <Route path="cabins" element={<Cabins />} />
+            <Route path="account" element={<Account />} />
+            <Route path="new" element={<NewUsers />} />
+          </Route>
           <Route path="*" element={<PageNotFound />} />
-          <Route path="account" element={<Account />} />
-          <Route path="new" element={<NewUsers />} />
+          <Route path="login" element={<Login />} />
         </Routes>
       </Router>
     </>
