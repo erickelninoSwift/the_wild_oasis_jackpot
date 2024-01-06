ae55b62f14e8174448db7d1bba5f178dac06ee4a
Erick El nino <erick.tshimbombo@yahoo.com>
2024-01-06 20:26:58 +0200

    update

--- src/ui/Sidebar.jsx
+++ src/ui/Sidebar.jsx
@@ -6,6 +6,9 @@ const StyleSidebar = styled.aside`
   padding: 3rem;
   border-right: 1px solid var(--color-grey-100);
   grid-row: 1 /-1;
+  display: flex;
+  flex-direction: column;
+  gap: 4.2rem;
 `;
 
 function Sidebar() {
