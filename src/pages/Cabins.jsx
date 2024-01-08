9507e0e279bcea3d8b47af496bf106d4005c1be8
Erick El nino <erick.tshimbombo@yahoo.com>
2024-01-08 04:52:33 +0200

    update finshed connecting our app with suparbase

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -1,11 +1,22 @@
+import { useEffect } from "react";
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
+import { getAllCabins } from "../services/apiCabins";
 
 function Cabins() {
+  useEffect(() => {
+    getAllCabins().then((data) => {
+      console.log(data);
+    });
+  }, []);
   return (
     <Row type="horizontal">
       <Heading as="h1">All cabins</Heading>
       <p>TEST</p>
+      <img
+        src="https://kjoutcsaoqibenvvyyni.supabase.co/storage/v1/object/public/cabins-images/cabin-002.jpg?t=2024-01-08T02%3A44%3A08.629Z"
+        alt=""
+      />
     </Row>
   );
 }
---
+++ src/services/apiCabins.js
@@ -0,0 +1,11 @@
+import supabase from "./supabase";
+
+export const getAllCabins = async () => {
+  let { data: cabins, error } = await supabase.from("cabins").select("*");
+  if (error) {
+    console.log(error);
+    throw new Error("there was an error while collecting all data");
+  }
+
+  return cabins;
+};
